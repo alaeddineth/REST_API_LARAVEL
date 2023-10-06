3b14370298bb7c9d83ce5eb5f717bfb061c0ee0e
alaeddineth <theljanialaeddine@gmail.com*>
2023-10-06 10:02:22 +0200

    "get_instructor_data"

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-    Route::get('onpc/personnel', function () {
-        return response()->json([
-            'name' => 'theljani',
-            'surname' => 'Alaeddine',
-            'Grade' => 'lieutenant',
-        ]);
-    });
     
+    
+});
+Route::get('Training/Instructors', function () {
+    return response()->json([
+        'name' => 'theljani',
+        'surname' => 'Alaeddine',
+        'job' => 'IT instructor & Full Stack Developer',
+    ]);
 });
